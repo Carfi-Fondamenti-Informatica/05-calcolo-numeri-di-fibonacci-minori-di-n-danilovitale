d26991c6941fc810e2d3d081ab53be19aaf97336
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-11-24 22:06:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main (){
+    int n = 0;
+    cin >> n;
+      if (n >= 2)
+	   {
+         int a1 = 1,a2 = 1;
+	      cout << a1 << endl<< a2 <<endl;
+	      int fib = 0,s = 0 ;
+	      while (1)
+		  {
+		     fib = a1 + a2;
+		     s = a1 + a2;
+		     if (fib<n)
+		       {
+		        cout << fib << endl;
+		       }
+		     else
+		       {
+		         break;
+		       }
+		     a1 = a2;
+		     a2 = s;
+	      }
+	  }
+      else
+	  {
+	  cout << "errore" << endl;
+	  }
+  return 0;
 }
